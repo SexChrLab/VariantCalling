6ec50e8979f5dcc25b788f954cf9031a4074678d
Angela Taravella <angelat1020@gmail.com>
2020-04-18 08:07:00 -0700

    edited fastqsToBams scripts

--- scripts/run.fastqsToBams.snakefile.sh
+++ scripts/run.fastqsToBams.snakefile.sh
@@ -8,7 +8,7 @@
 #SBATCH -t 96:00:00
 cd /home/amtarave/projects/Kenya_sequencing/whole_genome/
 source activate kenya
-snakemake -s fastqsToBams.snakefile -j 30 --latency-wait 60 --cluster-config fastqsToBams.cluster.json --cluster "sbatch -n {fastqsToBams.cluster.n} --nodes 1 -t {fastqsToBams.cluster.t}:00:00 --mail-type=END,FAIL --mail-user=amtarave@asu.edu "
+snakemake -s fastqsToBams.snakefile -j 30 --latency-wait 60 --cluster-config cluster.json --cluster "sbatch -n {cluster.n} --nodes 1 -t {cluster.t}:00:00 --mail-type=END,FAIL --mail-user=amtarave@asu.edu "
 
 mv slurm* logs_slurm/
 
